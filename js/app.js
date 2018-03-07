db041eae9a93d17c7fb717b11ef1773c39a51a81
andres nowak <andresnowak@andress-MacBook-Pro.local>
2018-03-06 21:56:26 -0600

    Feat: added the function openCard

--- js/app.js
+++ js/app.js
@@ -10,13 +10,36 @@
  *   - add each card's HTML to the page
  */
 
+function startGame() {
+  var listOfCards = $('.deck').find('li');
+  var shufledListOfCards = shuffle(listOfCards);
+
+  listOfCards.remove();
+  $('.deck').append(shufledListOfCards);
+  openCard();
+}
+
+$(startGame);
+
+function openCard() {
+  var deck = $('.deck');
+  var card = $('.card');
+
+  card.on('click', function(){
+    $(this).addClass('show open');
+  });
+}
+
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
-
+    // While there remain elements to shuffle...
     while (currentIndex !== 0) {
+        // Pick a remaining element..
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
+
+        // And swap it with the current element.
         temporaryValue = array[currentIndex];
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
