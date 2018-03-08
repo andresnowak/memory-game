7bee5bbb120ca9e6044e22d8f39bf57843a9402a
andres nowak <andresnowak@andress-MacBook-Pro.local>
2018-03-07 18:33:47 -0600

    Feat: added the matchCards function

--- js/app.js
+++ js/app.js
@@ -9,15 +9,13 @@
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
- var deck = $('.deck');
- var card = $('.card');
 
 function startGame() {
   var listOfCards = $('.deck').find('li');
   var shufledListOfCards = shuffle(listOfCards);
 
   listOfCards.remove();
-  deck.append(shufledListOfCards);
+  $('.deck').append(shufledListOfCards);
 
   openCard();
 }
@@ -25,14 +23,47 @@ function startGame() {
 $(startGame);
 
 function openCard() {
-  card.on('click', function(){
+  //when called it will open the cards
+  var count = 0; //to count how many cards have been clicked
+  var cardsClicked = [];
+  $('.card').on('click', function(){
     $(this).addClass('show open');
+    count += 1;
+    cardsClicked.push($(this));
+
+    if (count === 2){
+      matchCards(cardsClicked);
+      count = 0;
+      cardsClicked = [];
+    }
   });
 }
 
-function closeCard() {
-      card.toggleClass("open", false);
-      card.toggleClass("show", false);
+function closeCard(cardsClicked) {
+  //when called it will remove the clases so it closes
+  cardsClicked.forEach(function (card){
+    $('.card').toggleClass("open", false);
+    $('.card').toggleClass("show", false);
+  });
+}
+
+function matchCards(cardsClicked) {
+  //it will see if the cards match
+  card1NameOfClass = cardsClicked[0].children('i');
+  card2NameOfClass = cardsClicked[1].children('i');
+
+  if (card1NameOfClass.attr('class') === card2NameOfClass.attr('class')){
+    cardsClicked.forEach(function (card){
+      card.addClass('match');
+    });
+  }
+  else {
+    $('.card').off('click');
+    setTimeout(function(){
+     closeCard(cardsClicked);
+     openCard();
+    }, 2000);
+  }
 }
 
 // Shuffle function from http://stackoverflow.com/a/2450976
