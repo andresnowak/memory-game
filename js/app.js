08c97c0d0cf46221fcfb7797238afb69df56efb8
Andres Nowak <andresnowak52@gmail.com>
2018-03-08 22:43:48 -0600

    Feat: added the function end game

--- js/app.js
+++ js/app.js
@@ -27,12 +27,12 @@ function openCard() {
   //when called it will open the cards
   var count = 0; //to count how many cards have been clicked
   var cardsClicked = [];
-  $('.card').on('click', function(){
+  $('.card').on('click', function() {
     $(this).addClass('show open');
     count += 1;
     cardsClicked.push($(this));
 
-    if (count === 2){
+    if (count === 2) {
       matchCards(cardsClicked);
       count = 0;
       cardsClicked = [];
@@ -43,7 +43,7 @@ function openCard() {
 
 function closeCard(cardsClicked) {
   //when called it will remove the clases so it closes
-  cardsClicked.forEach(function (card){
+  cardsClicked.forEach(function(card) {
     $('.card').toggleClass("open", false);
     $('.card').toggleClass("show", false);
   });
@@ -51,27 +51,28 @@ function closeCard(cardsClicked) {
 
 var countOfHowManyIncorrectMatches = 0;
 var countOfHowManyCorrectMatches = 0;
+
 function matchCards(cardsClicked) {
   //it will see if the cards match
   var card1NameOfClass = cardsClicked[0].children('i');
   var card2NameOfClass = cardsClicked[1].children('i');
 
-  if (card1NameOfClass.attr('class') === card2NameOfClass.attr('class')){
-    cardsClicked.forEach(function (card){
+  if (card1NameOfClass.attr('class') === card2NameOfClass.attr('class')) {
+    cardsClicked.forEach(function(card) {
       card.addClass('match');
     });
-    countOfHowManyCorrectMatches+=1;
-    endGame(countOfHowManyCorrectMatches);
+    countOfHowManyCorrectMatches += 1;
+    endGame(countOfHowManyCorrectMatches, countOfHowManyIncorrectMatches);
   } else {
     $('.card').off('click');
 
-    setTimeout(function(){
-     closeCard(cardsClicked);
-     openCard();
-   }, 1500);
+    setTimeout(function() {
+      closeCard(cardsClicked);
+      openCard();
+    }, 1500);
 
-      countOfHowManyIncorrectMatches+=1;
-      emptyStar(countOfHowManyIncorrectMatches);
+    countOfHowManyIncorrectMatches += 1;
+    emptyStar(countOfHowManyIncorrectMatches);
   }
 }
 
@@ -98,6 +99,25 @@ function emptyStar(countOfIncorrectMatches) {
   }
 }
 
+function endGame(countOfCorrectMatches, countOfIncorrectMatches) {
+  moves = $(".moves").text();
+  localStorage.setItem('moves', moves);
+
+  if (countOfCorrectMatches === 8) {
+  window.location.replace("End_game_page.html");
+
+    if (countOfIncorrectMatches >= 10 && countOfIncorrectMatches < 16) {
+      localStorage.setItem('stars','2');
+    } else if (countOfIncorrectMatches >= 16 && countOfIncorrectMatches < 22) {
+      localStorage.setItem('stars', '1');
+    } else if (countOfIncorrectMatches >= 22) {
+      localStorage.setItem('stars', '0');
+    } else {
+      localStorage.setItem('stars', '3');
+    }
+  }
+}
+
 function restart() {
   var repeat = $('.fa-repeat');
   repeat.click(function() {
@@ -107,18 +127,19 @@ function restart() {
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-    // While there remain elements to shuffle...
-    while (currentIndex !== 0) {
-        // Pick a remaining element..
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
+  var currentIndex = array.length,
+    temporaryValue, randomIndex;
+  // While there remain elements to shuffle...
+  while (currentIndex !== 0) {
+    // Pick a remaining element..
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
 
-        // And swap it with the current element.
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
 
-    return array;
+  return array;
 }
